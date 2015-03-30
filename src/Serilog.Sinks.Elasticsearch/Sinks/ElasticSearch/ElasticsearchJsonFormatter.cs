4662aada014db3cba6d381c934fd747bb8f7e8d8
Michiel van Oudheusden <m.vanoudheusden@aerdata.com>
2015-03-30 17:52:08 +0200

    Handled .NET 4 code.

--- src/Serilog.Sinks.Elasticsearch/Sinks/ElasticSearch/ElasticsearchJsonFormatter.cs
+++ src/Serilog.Sinks.Elasticsearch/Sinks/ElasticSearch/ElasticsearchJsonFormatter.cs
@@ -61,6 +61,34 @@ namespace Serilog.Sinks.Elasticsearch
             _inlineFields = inlineFields;      
         }
 
+#if NET4
+        /// <summary>
+        /// Writes out individual renderings of attached properties
+        /// </summary>
+        protected override void WriteRenderings(IGrouping<string, PropertyToken>[] tokensWithFormat, IDictionary<string, LogEventPropertyValue> properties, TextWriter output)
+        {
+            output.Write(",\"{0}\":{{", "renderings");
+            WriteRenderingsValues(tokensWithFormat, properties, output);
+            output.Write("}");
+        }
+
+        /// <summary>
+        /// Writes out the attached properties
+        /// </summary>
+        protected override void WriteProperties(IDictionary<string, LogEventPropertyValue> properties, TextWriter output)
+        {
+            if (!_inlineFields)
+                output.Write(",\"{0}\":{{", "fields");
+            else
+                output.Write(",");
+
+            WritePropertiesValues(properties, output);
+
+            if (!_inlineFields)
+                output.Write("}");
+        }
+
+#else
         /// <summary>
         /// Writes out individual renderings of attached properties
         /// </summary>
@@ -86,6 +114,8 @@ namespace Serilog.Sinks.Elasticsearch
             if (!_inlineFields)
                 output.Write("}");
         }
+
+#endif
 
         /// <summary>
         /// Writes out the attached exception
@@ -168,7 +198,9 @@ namespace Serilog.Sinks.Elasticsearch
             this.WriteJsonProperty("Name", name, ref delim, output);
             this.WriteJsonProperty("AssemblyName", an.Name, ref delim, output);
             this.WriteJsonProperty("AssemblyVersion", an.Version.ToString(), ref delim, output);
+            #if !NET4
             this.WriteJsonProperty("AssemblyCulture", an.CultureName, ref delim, output);
+            #endif
             this.WriteJsonProperty("ClassName", className, ref delim, output);
             this.WriteJsonProperty("Signature", signature, ref delim, output);
             this.WriteJsonProperty("MemberType", memberType, ref delim, output);
