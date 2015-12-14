dbeef5761734bf47e4b30cd8b313ea523a361bd0
Michiel van Oudheusden <m.vanoudheusden@aerdata.com>
2015-12-14 22:45:23 +0100

    Added a simple command line sample for the sink.

---
+++ sample/Serilog.Sinks.Elasticsearch.Sample/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serilog.Sinks.Elasticsearch.Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Serilog.Debugging.SelfLog.Out = Console.Out;
+
+            Log.Logger = new LoggerConfiguration()
+               .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri("http://localhost:9200"))
+               {
+                   AutoRegisterTemplate = true
+               })
+               .MinimumLevel.Debug()
+               .CreateLogger();
+
+            Log.Debug("Debug");
+            Log.Information("Info");
+            Log.Warning("Warning");
+            Log.Error("Error");
+
+            Console.ReadLine();
+        }
+    }
+}
