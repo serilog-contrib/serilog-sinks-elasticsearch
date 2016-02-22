239baa7c9ecc0470a51424ee6b6390eba218298e
Lars Tabro Sørensen <lsrensen@ebay.com>
2016-02-22 17:22:41 +0100

    Fixed failing unittests. Added a test data helper to load embedded resources, since the present implementation of reading from files were pretty overly complicated.

---
+++ test/Serilog.Sinks.Elasticsearch.Tests/TestDataHelper.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serilog.Sinks.Elasticsearch.Tests
+{
+    public static class TestDataHelper
+    {
+        public static string ReadEmbeddedResource(
+            Assembly assembly,
+            string embeddedResourceNameEndsWith)
+        {
+            var resourceNames = assembly.GetManifestResourceNames();
+            var resourceName = resourceNames.SingleOrDefault(n => n.EndsWith(embeddedResourceNameEndsWith));
+
+            if (string.IsNullOrEmpty(resourceName))
+            {
+                throw new ArgumentException(
+                    string.Format(
+                        "Could not find embedded resouce name that ends with '{0}', only found these: {1}",
+                        embeddedResourceNameEndsWith,
+                        string.Join(", ", resourceNames)),
+                    "embeddedResourceNameEndsWith");
+            }
+
+            using (var stream = assembly.GetManifestResourceStream(resourceName))
+            {
+                if (stream == null)
+                {
+                    throw new ArgumentException(
+                        string.Format("Failed to open embedded resource stream for resource '{0}'", resourceName));
+                }
+
+                using (var streamReader = new StreamReader(stream))
+                {
+                    return streamReader.ReadToEnd();
+                }
+            }
+        }
+    }
+}
