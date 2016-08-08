560ed6e84605a1160cc0d9634cf1327498cadd71
Michiel van Oudheusden <m.vanoudheusden@aerdata.com>
2016-08-08 13:23:25 +0200

    Changed the build script to use gitversion for versioning.

--- Build.ps1
+++ Build.ps1
@@ -1,19 +1,19 @@
-
-$branch = @{ $true = $env:APPVEYOR_REPO_BRANCH; $false = $(git symbolic-ref --short -q HEAD) }[$env:APPVEYOR_REPO_BRANCH -ne $NULL];
-$revision = @{ $true = "{0:00000}" -f [convert]::ToInt32("0" + $env:APPVEYOR_BUILD_NUMBER, 10); $false = "local" }[$env:APPVEYOR_BUILD_NUMBER -ne $NULL];
-$suffix = @{ $true = ""; $false = "$($branch.Substring(0, [math]::Min(10,$branch.Length)))-$revision"}[$branch -eq "master" -and $revision -ne "local"]
-
 $solution = "$project.sln"
 $test = "test\\Serilog.Sinks.Elasticsearch.Tests\\project.json"
-$project = "src\\Serilog.Sinks.Elasticsearch\\project.json"
+$projectFolder = "src\\Serilog.Sinks.Elasticsearch"
+$project = $projectFolder + "\\project.json"
 
 function Invoke-Build()
 {
-    Write-Output "Building $suffix"
+    Write-Output "Building"
 
     & dotnet restore $test --verbosity Warning
     & dotnet restore $project --verbosity Warning
-
+	
+	# calculate version
+	& cd $projectFolder 
+	& dotnet gitversion $project --verbosity Warning
+	& cd "..\\.."
 
     & dotnet test $test -c Release
     if($LASTEXITCODE -ne 0) 
@@ -21,20 +21,15 @@ function Invoke-Build()
         Write-Output "The tests failed"
         exit 1 
     }
-    if ($suffix -ne "")
-    {
-        & dotnet pack $project -c Release -o .\artifacts  --version-suffix=$suffix
-    }
-    else 
-    {
-        & dotnet pack $project -c Release -o .\artifacts 
-    }
+  
+    & dotnet pack $project -c Release -o .\artifacts 
+  
     if($LASTEXITCODE -ne 0) 
     {
         Write-Output "Packing the sink failed"
         exit 1 
     }
-    Write-Output "Building $suffix done"
+    Write-Output "Building done"
 }
 
 $ErrorActionPreference = "Stop"
