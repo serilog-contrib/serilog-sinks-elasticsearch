serilog-contrib/serilog-sinks-elasticsearch

commit 144369ba189fa808fc88663ae20f11e57a9a1ba2

Added support for controlling the op_type when indexing (#356)
* Added support for controlling the op_type when indexing

In order to write to data streams we'll need to set the
op_type to create the default is index

In order to don't serialize null values for the batch action and to not depend
on custom serialization setting we're using LowLevelRequestResponseSerializer.Instance
for serialization of the action.

We won't throw exception when setting the TypeName to null since:
- This is deprecated in v7
- Won't work in v8

* Consider op_type when parsing response from bulk action

* Don't overwrite TypeName if it has been set to null

In order to be able to use templates and at the same time remove
the doc type we shouldn't override it if it's set to null.

* Support for setting the op_type for durable sink

Use the same logic for creating bulk action within
the durable sink as in the standard sink.

* Added change log

src/Serilog.Sinks.Elasticsearch/Sinks/ElasticSearch/ElasticsearchSinkState.cs
@@ -69,14 +69,10 @@ namespace Serilog.Sinks.Elasticsearch
             if (string.IsNullOrWhiteSpace(options.IndexFormat)) throw new ArgumentException("options.IndexFormat");
             if (string.IsNullOrWhiteSpace(options.TemplateName)) throw new ArgumentException("options.TemplateName");
 
-            // Strip type argument if ESv7 since multiple types are not supported anymore
-            if (options.AutoRegisterTemplateVersion == AutoRegisterTemplateVersion.ESv7)
+            // Since TypeName is deprecated we shouldn't set it, if has been deliberately set to null.
+            if (options.TypeName == null && options.AutoRegisterTemplateVersion == AutoRegisterTemplateVersion.ESv7)
             {
                 options.TypeName = "_doc";
-            }
-            else
-            {
-                if (string.IsNullOrWhiteSpace(options.TypeName)) throw new ArgumentException("options.TypeName");
             }
 
             _templateName = options.TemplateName;

test/Serilog.Sinks.Elasticsearch.Tests/BulkActionTests.cs
@@ -1,0 +1,90 @@
+using System;
+using System.Linq;
+using FluentAssertions;
+using Serilog.Events;
+using Serilog.Parsing;
+using Xunit;
+
+namespace Serilog.Sinks.Elasticsearch.Tests
+{
+    public class BulkActionTests : ElasticsearchSinkTestsBase
+    {
+        [Fact]
+        public void DefaultBulkActionV7()
+        {
+            _options.IndexFormat = "logs";
+            _options.TypeName = "_doc";
+            _options.PipelineName = null;
+            using (var sink = new ElasticsearchSink(_options))
+            {
+                sink.Emit(ADummyLogEvent());
+                sink.Emit(ADummyLogEvent());
+            }
+
+            var bulkJsonPieces = this.AssertSeenHttpPosts(_seenHttpPosts, 2, 1);
+            const string expectedAction = @"{""index"":{""_type"":""_doc"",""_index"":""logs""}}";
+            bulkJsonPieces[0].Should().Be(expectedAction);
+        }
+        
+        [Fact]
+        public void DefaultBulkActionV8()
+        {
+            _options.IndexFormat = "logs";
+            _options.TypeName = null;
+            _options.PipelineName = null;
+            using (var sink = new ElasticsearchSink(_options))
+            {
+                sink.Emit(ADummyLogEvent());
+                sink.Emit(ADummyLogEvent());
+            }
+
+            var bulkJsonPieces = this.AssertSeenHttpPosts(_seenHttpPosts, 2, 1);
+            const string expectedAction = @"{""index"":{""_index"":""logs""}}";
+            bulkJsonPieces[0].Should().Be(expectedAction);
+        }
+        
+        [Fact]
+        public void BulkActionDataStreams()
+        {
+            _options.IndexFormat = "logs-my-stream";
+            _options.TypeName = null;
+            _options.PipelineName = null;
+            _options.BatchAction = ElasticOpType.Create;
+            
+            using (var sink = new ElasticsearchSink(_options))
+            {
+                sink.Emit(ADummyLogEvent());
+                sink.Emit(ADummyLogEvent());
+            }
+
+            var bulkJsonPieces = this.AssertSeenHttpPosts(_seenHttpPosts, 2, 1);
+            const string expectedAction = @"{""create"":{""_index"":""logs-my-stream""}}";
+            bulkJsonPieces[0].Should().Be(expectedAction);
+        }
+        
+        [Fact]
+        public void PipelineAction()
+        {
+            _options.IndexFormat = "logs-my-stream";
+            _options.TypeName = "_doc";
+            _options.PipelineName = "my-pipeline";
+            _options.BatchAction = ElasticOpType.Index;
+            
+            using (var sink = new ElasticsearchSink(_options))
+            {
+                sink.Emit(ADummyLogEvent());
+                sink.Emit(ADummyLogEvent());
+            }
+
+            var bulkJsonPieces = this.AssertSeenHttpPosts(_seenHttpPosts, 2, 1);
+            const string expectedAction = @"{""index"":{""_type"":""_doc"",""_index"":""logs-my-stream"",""pipeline"":""my-pipeline""}}";
+            bulkJsonPieces[0].Should().Be(expectedAction);
+        }
+
+        private static LogEvent ADummyLogEvent() {
+            return new LogEvent(DateTimeOffset.Now, LogEventLevel.Information, null,
+                new MessageTemplate("A template", Enumerable.Empty<MessageTemplateToken>()),
+                Enumerable.Empty<LogEventProperty>());
+        }
+    }
+}

src/Serilog.Sinks.Elasticsearch/LoggerConfigurationElasticSearchExtensions.cs
@@ -143,6 +143,7 @@ namespace Serilog
         /// <param name="failureSink">Sink to use when Elasticsearch is unable to accept the events. This is optionally and depends on the EmitEventFailure setting.</param>   
         /// <param name="singleEventSizePostingLimit"><see cref="ElasticsearchSinkOptions.SingleEventSizePostingLimit"/>The maximum length of an event allowed to be posted to Elasticsearch.default null</param>
         /// <param name="templateCustomSettings">Add custom elasticsearch settings to the template</param>
+        /// <param name="batchAction">Configures the OpType being used when inserting document in batch. Must be set to create for data streams.</param>
         /// <returns>LoggerConfiguration object</returns>
         /// <exception cref="ArgumentNullException"><paramref name="nodeUris"/> is <see langword="null" />.</exception>
         public static LoggerConfiguration Elasticsearch(
@@ -180,7 +181,8 @@ namespace Serilog
             ILogEventSink failureSink = null,
             long? singleEventSizePostingLimit = null,
             int? bufferFileCountLimit = null,
-            Dictionary<string,string> templateCustomSettings = null)
+            Dictionary<string,string> templateCustomSettings = null,
+            ElasticOpType batchAction = ElasticOpType.Index)
         {
             if (string.IsNullOrEmpty(nodeUris))
                 throw new ArgumentNullException(nameof(nodeUris), "No Elasticsearch node(s) specified.");
@@ -208,6 +210,7 @@ namespace Serilog
             }
 
             options.BatchPostingLimit = batchPostingLimit;
+            options.BatchAction = batchAction;
             options.SingleEventSizePostingLimit = singleEventSizePostingLimit;
             options.Period = TimeSpan.FromSeconds(period);
             options.InlineFields = inlineFields;

src/Serilog.Sinks.Elasticsearch/Sinks/ElasticSearch/Durable/Elasticsearch/DurableElasticsearchSink.cs
@@ -15,7 +15,6 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
-using Elasticsearch.Net;
 using Serilog.Core;
 using Serilog.Events;
 
@@ -55,15 +54,16 @@ namespace Serilog.Sinks.Elasticsearch.Durable
 
             
             var elasticSearchLogClient = new ElasticsearchLogClient(
-                 elasticLowLevelClient: _state.Client, 
-                cleanPayload: _state.Options.BufferCleanPayload);
+                elasticLowLevelClient: _state.Client, 
+                cleanPayload: _state.Options.BufferCleanPayload,
+                elasticOpType: _state.Options.BatchAction);
 
             var payloadReader = new ElasticsearchPayloadReader(
                  pipelineName: _state.Options.PipelineName,  
                  typeName:_state.Options.TypeName, 
                  serialize:_state.Serialize,  
-                 getIndexForEvent: _state.GetBufferedIndexForEvent
-                );
+                 getIndexForEvent: _state.GetBufferedIndexForEvent,
+                 elasticOpType: _state.Options.BatchAction);
 
             _shipper = new ElasticsearchLogShipper(
                 bufferBaseFilename: _state.Options.BufferBaseFilename,

src/Serilog.Sinks.Elasticsearch/Sinks/ElasticSearch/Durable/Elasticsearch/ElasticsearchLogClient.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.ExceptionServices;
-using System.Text;
-using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Elasticsearch.Net;
 using Serilog.Debugging;
@@ -17,17 +14,21 @@ namespace Serilog.Sinks.Elasticsearch.Durable
     {
         private readonly IElasticLowLevelClient _elasticLowLevelClient;
         private readonly Func<string, long?, string, string> _cleanPayload;
+        private readonly ElasticOpType _elasticOpType;
 
         /// <summary>
         /// 
         /// </summary>
         /// <param name="elasticLowLevelClient"></param>
         /// <param name="cleanPayload"></param>
+        /// <param name="elasticOpType"></param>
         public ElasticsearchLogClient(IElasticLowLevelClient elasticLowLevelClient,
-            Func<string, long?, string, string> cleanPayload)
+            Func<string, long?, string, string> cleanPayload,
+            ElasticOpType elasticOpType)
         {
             _elasticLowLevelClient = elasticLowLevelClient;
             _cleanPayload = cleanPayload;
+            _elasticOpType = elasticOpType;
         }
 
         public async Task<SentPayloadResult> SendPayloadAsync(List<string> payload)
@@ -85,7 +86,7 @@ namespace Serilog.Sinks.Elasticsearch.Durable
             bool hasErrors = false;
             foreach (dynamic item in items)
             {
-                var itemIndex = item?["index"];
+                var itemIndex = item?[ElasticsearchSink.BulkAction(_elasticOpType)];
                 long? status = itemIndex?["status"];
                 i++;
                 if (!status.HasValue || status < 300)

src/Serilog.Sinks.Elasticsearch/Sinks/ElasticSearch/Durable/Elasticsearch/ElasticsearchPayloadReader.cs
@@ -3,8 +3,7 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Elasticsearch.Net;
 
 namespace Serilog.Sinks.Elasticsearch.Durable
 {
@@ -17,6 +16,7 @@ namespace Serilog.Sinks.Elasticsearch.Durable
         private readonly string _typeName;
         private readonly Func<object, string> _serialize;
         private readonly Func<string, DateTime,string> _getIndexForEvent;
+        private readonly ElasticOpType _elasticOpType;
         private List<string> _payload;
         private int _count;
         private DateTime _date;
@@ -28,12 +28,15 @@ namespace Serilog.Sinks.Elasticsearch.Durable
         /// <param name="typeName"></param>
         /// <param name="serialize"></param>
         /// <param name="getIndexForEvent"></param>
-        public ElasticsearchPayloadReader(string pipelineName,string typeName, Func<object,string> serialize,Func<string,DateTime,string> getIndexForEvent)
+        /// <param name="elasticOpType"></param>
+        public ElasticsearchPayloadReader(string pipelineName, string typeName, Func<object, string> serialize,
+            Func<string, DateTime, string> getIndexForEvent, ElasticOpType elasticOpType)
         {
             _pipelineName = pipelineName;
             _typeName = typeName;
             _serialize = serialize;
             _getIndexForEvent = getIndexForEvent;
+            _elasticOpType = elasticOpType;
         }
 
         /// <summary>
@@ -80,18 +83,13 @@ namespace Serilog.Sinks.Elasticsearch.Durable
         protected override void AddToPayLoad(string nextLine)
         {
             var indexName = _getIndexForEvent(nextLine, _date);
-            var action = default(object);
+            var action = ElasticsearchSink.CreateElasticAction(
+                opType: _elasticOpType, 
+                indexName: indexName, pipelineName: _pipelineName,
+                id: _count + "_" + Guid.NewGuid(),
+                mappingType: _typeName);
+            var actionJson = LowLevelRequestResponseSerializer.Instance.SerializeToString(action);
 
-            if (string.IsNullOrWhiteSpace(_pipelineName))
-            {
-                action = new { index = new { _index = indexName, _type = _typeName, _id = _count + "_" + Guid.NewGuid() } };
-            }
-            else
-            {
-                action = new { index = new { _index = indexName, _type = _typeName, _id = _count + "_" + Guid.NewGuid(), pipeline = _pipelineName } };
-            }
-
-            var actionJson = _serialize(action);
             _payload.Add(actionJson);
             _payload.Add(nextLine);
             _count++;

src/Serilog.Sinks.Elasticsearch/Sinks/ElasticSearch/ElasticSearchSink.cs
@@ -16,9 +16,9 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Threading.Tasks;
 using Elasticsearch.Net;
-using Elasticsearch.Net.Specification.SecurityApi;
 using Serilog.Debugging;
 using Serilog.Events;
 using Serilog.Sinks.PeriodicBatching;
@@ -82,7 +82,7 @@ namespace Serilog.Sinks.Elasticsearch
             if (events == null || !events.Any())
                 return Task.FromResult<T>(default(T));
 
-            var payload = CreatePlayLoad<T>(events);
+            var payload = CreatePlayLoad(events);
             return _state.Client.BulkAsync<T>(PostData.MultiJson(payload));
         }
 
@@ -97,7 +97,7 @@ namespace Serilog.Sinks.Elasticsearch
             if (events == null || !events.Any())
                 return null;
 
-            var payload = CreatePlayLoad<T>(events);
+            var payload = CreatePlayLoad(events);
             return _state.Client.Bulk<T>(PostData.MultiJson(payload));
         }
 
@@ -165,8 +165,7 @@ namespace Serilog.Sinks.Elasticsearch
             return settings.GetType().GetProperty(name) != null;
         }
 
-        private IEnumerable<string> CreatePlayLoad<T>(IEnumerable<LogEvent> events)
-            where T : class, IElasticsearchResponse, new()
+        private IEnumerable<string> CreatePlayLoad(IEnumerable<LogEvent> events)
         {
             if (!_state.TemplateRegistrationSuccess && _state.Options.RegisterTemplateFailure == RegisterTemplateRecovery.FailSink)
             {
@@ -177,19 +176,15 @@ namespace Serilog.Sinks.Elasticsearch
             foreach (var e in events)
             {
                 var indexName = _state.GetIndexForEvent(e, e.Timestamp.ToUniversalTime());
-                var action = default(object);
-
                 var pipelineName = _state.Options.PipelineNameDecider?.Invoke(e) ?? _state.Options.PipelineName;
-                if (string.IsNullOrWhiteSpace(pipelineName))
-                {
-                    action = new { index = new { _index = indexName, _type = _state.Options.TypeName } };
-                }
-                else
-                {
-                    action = new { index = new { _index = indexName, _type = _state.Options.TypeName, pipeline = pipelineName } };
-                }
-                var actionJson = _state.Serialize(action);
-                payload.Add(actionJson);
+
+                var action = CreateElasticAction(
+                    opType: _state.Options.BatchAction, 
+                    indexName: indexName,
+                    pipelineName: pipelineName, 
+                    mappingType: _state.Options.TypeName);
+                payload.Add(LowLevelRequestResponseSerializer.Instance.SerializeToString(action));
+
                 var sw = new StringWriter();
                 _state.Formatter.Format(e, sw);
                 payload.Add(sw.ToString());
@@ -204,10 +199,15 @@ namespace Serilog.Sinks.Elasticsearch
             if (result.Success && result.Body?["errors"] == true)
             {
                 var indexer = 0;
+                var opType = BulkAction(_state.Options.BatchAction);
                 var items = result.Body["items"];
                 foreach (var item in items)
                 {
-                    if (item["index"] != null && HasProperty(item["index"], "error") && item["index"]["error"] != null)
+                    var action = item.ContainsKey(opType)
+                        ? item[opType]
+                        : null;
+                    
+                    if (action != null && action.ContainsKey("error"))
                     {
                         var e = events.ElementAt(indexer);
                         if (_state.Options.EmitEventFailure.HasFlag(EmitEventFailureHandling.WriteToSelfLog))
@@ -216,8 +216,8 @@ namespace Serilog.Sinks.Elasticsearch
                             SelfLog.WriteLine(
                                 "Failed to store event with template '{0}' into Elasticsearch. Elasticsearch reports for index {1} the following: {2}",
                                 e.MessageTemplate,
-                                item["index"]["_index"],
-                                _state.Serialize(item["index"]["error"]));
+                                action["_index"],
+                                _state.Serialize(action["error"]));
                         }
 
                         if (_state.Options.EmitEventFailure.HasFlag(EmitEventFailureHandling.WriteToFailureSink) &&
@@ -251,7 +251,6 @@ namespace Serilog.Sinks.Elasticsearch
                                     _state.Options.FailureCallback);
                             }
                         }
-
                     }
                     indexer++;
                 }
@@ -260,6 +259,70 @@ namespace Serilog.Sinks.Elasticsearch
             {
                 HandleException(result.OriginalException, events);
             }
+        }
+
+        internal static string BulkAction(ElasticOpType elasticOpType) =>
+            elasticOpType == ElasticOpType.Create
+                ? "create"
+                : "index";
+        
+        internal static object CreateElasticAction(ElasticOpType opType, string indexName, string pipelineName = null, string id = null, string mappingType = null)
+        {
+            var actionPayload = new ElasticActionPayload(
+                indexName: indexName,
+                pipeline: string.IsNullOrWhiteSpace(pipelineName) ? null : pipelineName,
+                id: id,
+                mappingType: mappingType
+            );
+
+            var action = opType == ElasticOpType.Create
+                ? (object) new ElasticCreateAction(actionPayload)
+                : new ElasticIndexAction(actionPayload);
+            return action;
+        }
+        
+        sealed class ElasticCreateAction
+        {
+            public ElasticCreateAction(ElasticActionPayload payload)
+            {
+                Payload = payload;
+            }
+
+            [DataMember(Name = "create")]
+            public ElasticActionPayload Payload { get; }
+        }
+
+        sealed class ElasticIndexAction
+        {
+            public ElasticIndexAction(ElasticActionPayload payload)
+            {
+                Payload = payload;
+            }
+
+            [DataMember(Name = "index")]
+            public ElasticActionPayload Payload { get; }
+        }
+
+        sealed class ElasticActionPayload {
+            public ElasticActionPayload(string indexName, string pipeline = null, string id = null, string mappingType = null)
+            {
+                IndexName = indexName;
+                Pipeline = pipeline;
+                Id = id;
+                MappingType = mappingType;
+            }
+
+            [DataMember(Name = "_type")]
+            public string MappingType { get; }
+
+            [DataMember(Name = "_index")]
+            public string IndexName { get; }
+
+            [DataMember(Name = "pipeline")]
+            public string Pipeline { get; }
+            
+            [DataMember(Name = "_id")]
+            public string Id { get; }
         }
     }
 }

src/Serilog.Sinks.Elasticsearch/Sinks/ElasticSearch/ElasticsearchSinkOptions.cs
@@ -15,6 +15,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using Elasticsearch.Net;
 using Serilog.Core;
 using Serilog.Events;
@@ -112,6 +113,12 @@ namespace Serilog.Sinks.Elasticsearch
         /// The default elasticsearch type name to use for the log events. Defaults to: logevent.
         /// </summary>
         public string TypeName { get; set; }
+
+        /// <summary>
+        /// Configures the <see cref="OpType"/> being used when bulk indexing documents.
+        /// In order to use data streams, this needs to be set to OpType.Create. 
+        /// </summary>
+        public ElasticOpType BatchAction { get; set; } = ElasticOpType.Index;
 
         /// <summary>
         /// Function to decide which Pipeline to use for the LogEvent
@@ -402,5 +409,26 @@ namespace Serilog.Sinks.Elasticsearch
         /// When the template cannot be registered, throw an exception and fail the sink.
         /// </summary>
         FailSink = 8
+    }
+
+    /// <summary>
+    /// Collection of op_type:s that can be used when indexing documents
+    /// ‹https://www.elastic.co/guide/en/elasticsearch/reference/current/docs-index_.html
+    ///
+    /// This is the same as the internal <seealso cref="OpType"/> but we don't want to
+    /// expose it in the API.
+    /// </summary>
+    public enum ElasticOpType
+    {
+        /// <summary>
+        /// Default option, creates or updates a document.
+        /// </summary>
+        [EnumMember(Value = "index")] Index,
+        /// <summary>
+        /// Set to create to only index the document if it does not already exist (put if absent).
+        /// - If a document with the specified _id already exists, the indexing operation will fail.
+        /// - If the request targets a data stream, an op_type of create is required
+        /// </summary>
+        [EnumMember(Value = "create")] Create
     }
 }

commit ab86ebe9e1f5699e2821b350cb70fd82020887cd

Override type name if specified
Fixes a regression introduced in #356
We should override the TypeName if it's specified.

Note that there are different defaults depending
on how the options are created.
- Created from configuration it defaults to `logevent`
- Created from code it defaults to `_doc`

src/Serilog.Sinks.Elasticsearch/Sinks/ElasticSearch/ElasticsearchSinkState.cs
@@ -70,7 +70,7 @@ namespace Serilog.Sinks.Elasticsearch
             if (string.IsNullOrWhiteSpace(options.TemplateName)) throw new ArgumentException("options.TemplateName");
 
             // Since TypeName is deprecated we shouldn't set it, if has been deliberately set to null.
-            if (options.TypeName == null && options.AutoRegisterTemplateVersion == AutoRegisterTemplateVersion.ESv7)
+            if (options.TypeName != null && options.AutoRegisterTemplateVersion == AutoRegisterTemplateVersion.ESv7)
             {
                 options.TypeName = "_doc";
             }

test/Serilog.Sinks.Elasticsearch.Tests/BulkActionTests.cs
@@ -25,6 +25,24 @@ namespace Serilog.Sinks.Elasticsearch.Tests
             const string expectedAction = @"{""index"":{""_type"":""_doc"",""_index"":""logs""}}";
             bulkJsonPieces[0].Should().Be(expectedAction);
         }
+
+        [Fact]
+        public void BulkActionV7OverrideTypeName()
+        {
+            _options.IndexFormat = "logs";
+            _options.TypeName = "logevent"; // This is the default value when creating the sink via configuration
+            _options.AutoRegisterTemplateVersion = AutoRegisterTemplateVersion.ESv7;
+            _options.PipelineName = null;
+            using (var sink = new ElasticsearchSink(_options))
+            {
+                sink.Emit(ADummyLogEvent());
+                sink.Emit(ADummyLogEvent());
+            }
+
+            var bulkJsonPieces = this.AssertSeenHttpPosts(_seenHttpPosts, 2, 1);
+            const string expectedAction = @"{""index"":{""_type"":""_doc"",""_index"":""logs""}}";
+            bulkJsonPieces[0].Should().Be(expectedAction);
+        }        
         
         [Fact]
         public void DefaultBulkActionV8()