1ddf4a4b50b8ed5514bd3b29f8553645851327a7
Andrey Kozlov <andrey.kozlov@skuvault.com>
2022-03-26 11:04:34 +0500

    Add tests for the desired functionality, which fail now.
    - Return code to support only Daily rolling interval
    - Add RollingIntervalExtensions.cs (origin: Serilog.Sinks.File) with InternalVisible attribute to be able to test
    - Add InternalVisible to FileSet.cs to be able to test it
    - Sign tests assembly the same way as Serilog.Sinks.Elasticsearch for InternalVisible to work

---
+++ test/Serilog.Sinks.Elasticsearch.Tests/ElasticsearchPayloadReaderTests.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using FluentAssertions;
+using Serilog.Sinks.Elasticsearch.Durable;
+using Xunit;
+
+namespace Serilog.Sinks.Elasticsearch.Tests;
+
+public class ElasticsearchPayloadReaderTests : IDisposable
+{
+    private readonly string _tempFileFullPathTemplate;
+    private string _bufferFileName;
+
+    public ElasticsearchPayloadReaderTests()
+    {
+        _tempFileFullPathTemplate = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString("N")) + "-{0}.json";
+    }
+
+    public void Dispose()
+    {
+        System.IO.File.Delete(_bufferFileName);
+    }
+
+    [Theory]
+    [InlineData(RollingInterval.Day)]
+    [InlineData(RollingInterval.Hour)]
+    [InlineData(RollingInterval.Infinite)]
+    [InlineData(RollingInterval.Minute)]
+    [InlineData(RollingInterval.Month)]
+    [InlineData(RollingInterval.Year)]
+    public void ElasticsearchPayloadReader_ReadPayload_ShouldReadSpecifiedTypesOfRollingFile(
+        RollingInterval rollingInterval)
+    {
+        // Arrange
+        var format = rollingInterval.GetFormat();
+        var payloadReader = new ElasticsearchPayloadReader("testPipelineName",
+            "TestTypeName",
+            null,
+            (_, _) => "TestIndex",
+            ElasticOpType.Index);
+        var lines = new[]
+        {
+            rollingInterval.ToString()
+        };
+        _bufferFileName = string.Format(_tempFileFullPathTemplate,
+            string.IsNullOrEmpty(format) ? string.Empty : new DateTime(2000, 1, 1).ToString(format));
+        // Important to use UTF8 with BOM if we are starting from 0 position 
+        System.IO.File.WriteAllLines(_bufferFileName, lines, new UTF8Encoding(true));
+
+        // Act
+        var fileSetPosition = new FileSetPosition(0, _bufferFileName);
+        var count = 0;
+        var payload =
+            payloadReader.ReadPayload(int.MaxValue,
+                null,
+                ref fileSetPosition,
+                ref count,
+                _bufferFileName);
+
+        // Assert
+        payload.Count.Should().Be(lines.Length * 2);
+        payload[1].Should().Be(lines[0]);
+    }
+}
---
+++ test/Serilog.Sinks.Elasticsearch.Tests/FileSetTests.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using FluentAssertions;
+using Serilog.Sinks.Elasticsearch.Durable;
+using Xunit;
+
+namespace Serilog.Sinks.Elasticsearch.Tests;
+
+public class FileSetTests : IDisposable
+{
+    private readonly string _fileNameBase;
+    private readonly string _tempFileFullPathTemplate;
+    private Dictionary<RollingInterval, string> _bufferFileNames;
+
+    public FileSetTests()
+    {
+        _fileNameBase = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString("N"));
+        _tempFileFullPathTemplate = _fileNameBase + "-{0}.json";
+    }
+
+    public void Dispose()
+    {
+        foreach (var bufferFileName in _bufferFileNames.Values) System.IO.File.Delete(bufferFileName);
+    }
+
+    [Theory]
+    [InlineData(RollingInterval.Day)]
+    [InlineData(RollingInterval.Hour)]
+    [InlineData(RollingInterval.Infinite)]
+    [InlineData(RollingInterval.Minute)]
+    [InlineData(RollingInterval.Month)]
+    [InlineData(RollingInterval.Year)]
+    public void GetBufferFiles_ReturnsOnlySpecifiedTypeOfRollingFile(RollingInterval rollingInterval)
+    {
+        // Arrange
+        var format = rollingInterval.GetFormat();
+        _bufferFileNames = GenerateFilesUsingFormat(format);
+        var fileSet = new FileSet(_fileNameBase);
+        var bufferFileForInterval = _bufferFileNames[rollingInterval];
+
+        // Act
+        var bufferFiles = fileSet.GetBufferFiles();
+
+        // Assert
+        bufferFiles.Should().BeEquivalentTo(bufferFileForInterval);
+    }
+
+    /// <summary>
+    ///     Generates buffer files for all RollingIntervals and returns dictionary of {rollingInterval, fileName} pairs.
+    /// </summary>
+    /// <param name="format"></param>
+    /// <returns></returns>
+    private Dictionary<RollingInterval, string> GenerateFilesUsingFormat(string format)
+    {
+        var result = new Dictionary<RollingInterval, string>();
+        foreach (var rollingInterval in Enum.GetValues(typeof(RollingInterval)))
+        {
+            var bufferFileName = string.Format(_tempFileFullPathTemplate,
+                string.IsNullOrEmpty(format) ? string.Empty : new DateTime(2000, 1, 1).ToString(format));
+            var lines = new[] {rollingInterval.ToString()};
+            // Important to use UTF8 with BOM if we are starting from 0 position 
+            System.IO.File.WriteAllLines(bufferFileName, lines, new UTF8Encoding(true));
+            result.Add((RollingInterval) rollingInterval, bufferFileName);
+        }
+
+        return result;
+    }
+}
